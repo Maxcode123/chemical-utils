3bd6cec59f43a1155cfe2c50e3a525ad5905f73a
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-04-18 09:59:14 +0300

    Add type_check argument to assert_result function

--- src/chemical_utils/tests/substances/substance_utils.py
+++ src/chemical_utils/tests/substances/substance_utils.py
@@ -1,6 +1,12 @@
+from typing import Any
+
 from chemical_utils.tests.base import TestBase
 
 
 class TestSubstances(TestBase):
-    def assert_result(self, result_str):
+    produced_type: Any
+
+    def assert_result(self, result_str, type_check=False):
         self.assertSequenceEqual(str(self.result()), result_str, str)
+        if type_check:
+            self.assertIsInstance(self.cachedResult(), self.produced_type)
