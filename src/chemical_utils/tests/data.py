ade287fdec0ff0867e029ece6f6f8babafafd899
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-04-19 20:01:23 +0300

    Add more test data

--- src/chemical_utils/tests/data.py
+++ src/chemical_utils/tests/data.py
@@ -1,4 +1,12 @@
-from chemical_utils.substances.substance import ChemicalElement
+from chemical_utils.substances.substance import (
+    ChemicalElement,
+    ChemicalElementTuple,
+    ChemicalCompound,
+)
 
 TESTIUM = ChemicalElement(10, 17.32, "Ts")
+TESTIUM2 = ChemicalElementTuple(TESTIUM, 2)
 PYTHONIUM = ChemicalElement(11, 23.09, "Py")
+PYTHONIUM3 = ChemicalElementTuple(PYTHONIUM, 3)
+ANACONDIUM = ChemicalElement(12, 29, "An")
+TS_PY_AN = ChemicalCompound(TESTIUM, PYTHONIUM, ANACONDIUM)
