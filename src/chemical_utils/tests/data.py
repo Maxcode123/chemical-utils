7decf465d9a8a836aa15fae95806f72b0fa8110b
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-05-21 14:08:37 +0300

    Adds reaction test data

--- src/chemical_utils/tests/data.py
+++ src/chemical_utils/tests/data.py
@@ -3,6 +3,12 @@ from chemical_utils.substances.substance import (
     ChemicalElementTuple,
     ChemicalCompound,
 )
+from chemical_utils.reactions.reaction import r
+from chemical_utils.properties.registry import (
+    create_standard_formation_properties,
+    create_standard_entropy,
+)
+from chemical_utils.properties.properties import MolarEnergy, Entropy
 
 TESTIUM = ChemicalElement(10, 17.32, "Ts")
 TESTIUM2 = ChemicalElementTuple(TESTIUM, 2)
@@ -12,3 +18,13 @@ ANACONDIUM = ChemicalElement(12, 29, "An")
 TS_PY = ChemicalCompound(TESTIUM, PYTHONIUM)
 TS2_PY3 = ChemicalCompound(TESTIUM2, PYTHONIUM3)
 TS_PY_AN = ChemicalCompound(TESTIUM, PYTHONIUM, ANACONDIUM)
+
+create_standard_formation_properties(TESTIUM2, MolarEnergy(0), MolarEnergy(0))
+create_standard_entropy(TESTIUM2, Entropy(50))
+create_standard_formation_properties(PYTHONIUM3, MolarEnergy(0), MolarEnergy(0))
+create_standard_entropy(PYTHONIUM3, Entropy(45))
+create_standard_formation_properties(TS2_PY3, MolarEnergy(100), MolarEnergy(200))
+create_standard_entropy(TS2_PY3, Entropy(200))
+
+reaction_1 = r(TESTIUM2 + PYTHONIUM3, TS2_PY3)
+reaction_2 = r(TESTIUM + PYTHONIUM, TS_PY)
