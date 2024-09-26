02f0bc1d10896203d6763dd2114f32a0222b569a
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-09-26 19:02:07 +0300

    Defines test class ChemicalReactionOperand

--- src/chemical_utils/tests/substances/substance_utils.py
+++ src/chemical_utils/tests/substances/substance_utils.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from chemical_utils.tests.base import TestBase
+from chemical_utils.exceptions.substances import ChemicalReactionOperandSubtractionError
 
 
 class TestSubstances(TestBase):
@@ -10,3 +11,13 @@ class TestSubstances(TestBase):
         self.assertSequenceEqual(str(self.result()), result_str, str)
         if type_check:
             self.assertIsInstance(self.cachedResult(), self.produced_type)
+
+
+class TestChemicalReactionOperand(TestSubstances):
+    def assert_operand(self, *factors, **kwargs):
+        self.assertCountEqual(self.result().factors, list(factors))
+        if kwargs.get("type_check", True):
+            self.assertIsInstance(self.cachedResult(), self.produced_type)
+
+    def assert_raises_subtraction_error(self):
+        self.assertResultRaises(ChemicalReactionOperandSubtractionError)
